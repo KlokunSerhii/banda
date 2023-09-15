8912e2cc2b20c1827012b4b34523016aaf347e45
Vira Zhuhan <115663875+ViraZhuhan@users.noreply.github.com>
2023-09-15 18:33:21 +0200

    start files

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,13 @@
+import Header from 'components/layout/Header';
+import UserBar from 'components/layout/UserBar';
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom/dist';
 
 function SharedLayout() {
   return (
     <div>
+      <Header/>
+      <UserBar/>
       <Suspense fallback={<b>LOAD...</b>}>
         <Outlet />
       </Suspense>
