4b8914e2a66b127664e93aaa9925ef9c5339f9d4
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-09-18 23:39:46 +0300

    Added container

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,55 +1,55 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Welcome.module.css';
-import bg from '../../images/side-view-people-training-gym 1.jpg';
-import bgPhone from '../../images/side-view-people-training-gym-phone.jpg';
+import symbolDefs from '../../images/symbol-defs.svg';
 import line from '../../images/Line.png';
-import polygon from '../../images/Polygon 1.png';
-import man from '../../images/Group.png';
+import Container from '../../components/Container';
 function Welcome() {
   return (
-    <div className={styles.containerWelcome}>
-      <div className={styles.infoWelcome}>
-        <img src={line} alt="line" className={styles.line} />
-        <h1 className={styles.title}>
-          Transforming your body shape with Power Pulse
-        </h1>
-        <div className={styles.buttonContainer}>
-          <NavLink className={styles.buttonUp} to="/singup">
-            <span className={styles.buttonText}>Sign Up</span>
-          </NavLink>
-          <NavLink className={styles.buttonIn} to="/singin">
-            <span className={styles.buttonText}>Sign In</span>
-          </NavLink>
-        </div>
-      </div>
-      <div className={styles.imgContainer}>
-        {/* <img src={bg} alt="bg" className={styles.bg} /> */}
+    <section className={styles.backGround}>
+      <Container>
+        <div className={styles.screen}>
+          <div className={styles.videoCountBox}>
+            <div className={styles.videoCountBack}>
+              <svg className={styles.videoPlayIcon} width="12" height="12">
+                <use href={symbolDefs + '#play-symbol-icon'}></use>
+              </svg>
+            </div>
+            <div>
+              <p className={styles.videoCount}>350+</p>
+              <p className={styles.videoTutorial}>Video tutorial</p>
+            </div>
+          </div>
 
-        <picture className={styles.bg}>
-          <source srcSet={bgPhone} media="(max-width:1200px)" />
-          <source srcSet={bg} media="(max-width:1200px)" />
-          <img className={styles.bg} src={bg} alt="Bowl with ice cream" />
-        </picture>
+          <div className={styles.caloriesCountBox}>
+            <div className={styles.caloriesCountBack}>
+              <svg className={styles.runIcon} width="17" height="17">
+                <use href={symbolDefs + '#run-icon'}></use>
+              </svg>
+            </div>
+            <div className={styles.caloriesContent}>
+              <p className={styles.caloriesCount}>500</p>
+              <p className={styles.caloriesTutorial}>cal</p>
+            </div>
+          </div>
 
-        <div className={styles.containerVideo}>
-          <div className={styles.elips}>
-            <img src={polygon} alt="polygon" className={styles.polygon} />
+          <div className={styles.infoWelcome}>
+            <img src={line} alt="line" className={styles.line} />
+            <h1 className={styles.title}>
+              Transforming your body shape with Power Pulse
+            </h1>
+            <div className={styles.buttonContainer}>
+              <NavLink className={styles.buttonUp} to="/singup">
+                <span className={styles.buttonText}>Sign Up</span>
+              </NavLink>
+              <NavLink className={styles.buttonIn} to="/singin">
+                <span className={styles.buttonText}>Sign In</span>
+              </NavLink>
+            </div>
           </div>
-          <p className={styles.videoNumber}>350+</p>
-          <p className={styles.videoText}>Video tutorial</p>
         </div>
-        <div className={styles.containerCalorie}>
-          <div className={styles.elipsRun}>
-            <img src={man} alt="man" className={styles.man} />
-          </div>
-          <div>
-            <p className={styles.calNumber}>500</p>
-            <p className={styles.calText}>cal</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      </Container>
+    </section>
   );
 }
 
