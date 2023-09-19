7a6d415d7fd5db9c7b748c9bef7f0089ed6b015f
Serhii Klokun <klokuns@gmail.com>
2023-09-19 22:23:28 +0300

    products list

--- src/components/products/ProductsFilters/ProductsFilters.jsx
+++ src/components/products/ProductsFilters/ProductsFilters.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
-import styles from "./ProductsFilter.module.css";
+import styles from './ProductsFilter.module.css';
 import { useState } from 'react';
-import { BiSearch } from "react-icons/bi";
-import { IoClose, IoChevronDown } from "react-icons/io5";
-
+import { BiSearch } from 'react-icons/bi';
+import { IoClose, IoChevronDown } from 'react-icons/io5';
 
 const categories = [
-  "alcoholic drinks",
-  "berries",
-  "cereals",
-  "dairy",
-  "dried fruits",
-  "eggs",
-  "fish",
-  "flour",
-  "fruits",
-  "meat",
-  "mushrooms",
-  "nuts",
-  "oils and fats",
-  "poppy",
-  "sausage",
-  "seeds",
-  "sesame",
-  "soft drinks",
-  "vegetables and herbs"
+  'alcoholic drinks',
+  'berries',
+  'cereals',
+  'dairy',
+  'dried fruits',
+  'eggs',
+  'fish',
+  'flour',
+  'fruits',
+  'meat',
+  'mushrooms',
+  'nuts',
+  'oils and fats',
+  'poppy',
+  'sausage',
+  'seeds',
+  'sesame',
+  'soft drinks',
+  'vegetables and herbs',
 ];
 
-const filters = [
-  "All",
-  "Reommended",
-  "Not Recommended"
-];
+const filters = ['All', 'Reommended', 'Not Recommended'];
 
 function ProductsFilters() {
   const [focus, setFocus] = useState(false);
@@ -40,108 +35,150 @@ function ProductsFilters() {
   const [showCategories, setShowCategories] = useState(false);
   const [showFilters, setShowFilters] = useState(false);
 
-
-  const handleChange = (e) => {
+  const handleChange = e => {
     setInputValue(e.target.value);
+  };
 
-  }
-
-  const handleInputChange = (e) => {
+  const handleInputChange = e => {
     setInputValue(e.target.value);
 
     if (e.target.value !== '') {
       setFocus(true);
-      
-      console.log(inputValue)
-      return;
-  } 
 
-    
-    return setFocus(false)
-    
-  }
+      console.log(inputValue);
+      return;
+    }
+
+    return setFocus(false);
+  };
 
   const handleClose = () => {
     // setQuery('');
     setInputValue('');
     console.log(inputValue);
-  }
+  };
 
-  const handleSearch = (e) => {
+  const handleSearch = e => {
     // setQuery('');
     setInputValue('');
-    console.log("search");
-  }
+    console.log('search');
+  };
 
   const handleShowCategories = () => {
     setShowCategories(!showCategories);
-    console.log("show modal categories");
-  }
+    console.log('show modal categories');
+  };
 
   const handleShowFilter = () => {
     setShowFilters(!showFilters);
-    console.log("show modal filter");
-  }
+    console.log('show modal filter');
+  };
 
-  
   return (
-    <div className={styles.wrapper}>
-      <h2 className={styles.title}>Products</h2>
-      <p  className={styles.description}>Filters</p>
-      <div className={styles.searchWrapper}>
-        <input className={styles.searchBar} type='text' placeholder='Search' onChange={handleInputChange}></input>
-        {focus && <button className={styles.closeBtn} type="button" onClick={handleClose}><IoClose style={{
-          fill: "#E6533C", width: "21px", height: "21px"
-        }} /></button>}
-        <button className={styles.searchBtn} type='submit' onSubmit={handleSearch}><BiSearch style={{ fill: "#EFEDE8",
-          width: "18px", height: "18px"
-        }} /></button>
-      
-    
-      </div>
+    <div className={styles.containetFilter}>
+      <p className={styles.description}>Filters</p>
 
-       <div className={styles.box}>
-         <div className={styles.categoriesWrapper}>
-        
-        <input className={styles.categoriesBar} type='text' placeholder='Categories' onChange={handleChange} />
-        <button className={styles.chevronBtn} onClick={handleShowCategories}>
-          <IoChevronDown style={{
-            color:"#EFEDE8",
-          width:"18px", height:"18px"}}/>
-          </button> 
-          
-        </div>
-       {showCategories &&  <ul className={styles.categoriesList}>
-          {categories.map(category => {
-            return <li className={styles.categoriesListItem} key={category} >{category}</li>
-          })}
-        </ul>}
-      </div> 
-     
-    
-
-      <div className={styles.box}>
-        <div className={styles.filtersWrapper}>
-          <input className={styles.filtersBar} type='text' placeholder='Filter' onChange={handleChange} />
-        <button className={styles.chevronBtn} onClick={handleShowFilter}>
-          <IoChevronDown style={{
-          color:"#EFEDE8",
-          width:"18px", height:"18px"}}/>
+      <div className={styles.wrapper}>
+        <div className={styles.searchWrapper}>
+          <input
+            className={styles.searchBar}
+            type="text"
+            placeholder="Search"
+            onChange={handleInputChange}
+          ></input>
+          {focus && (
+            <button
+              className={styles.closeBtn}
+              type="button"
+              onClick={handleClose}
+            >
+              <IoClose
+                style={{
+                  fill: '#E6533C',
+                  width: '21px',
+                  height: '21px',
+                }}
+              />
+            </button>
+          )}
+          <button
+            className={styles.searchBtn}
+            type="submit"
+            onSubmit={handleSearch}
+          >
+            <BiSearch
+              style={{ fill: '#EFEDE8', width: '18px', height: '18px' }}
+            />
           </button>
-          
         </div>
-        {showFilters && <ul className={styles.filtersList}>
-          {filters.map(filter => {
-            return <li className={styles.filtersListItem} key={filter} >{filter}</li>
-          })}
-        </ul>}
+
+        <div className={styles.box}>
+          <div className={styles.categoriesWrapper}>
+            <input
+              className={styles.categoriesBar}
+              type="text"
+              placeholder="Categories"
+              onChange={handleChange}
+            />
+            <button
+              className={styles.chevronBtn}
+              onClick={handleShowCategories}
+            >
+              <IoChevronDown
+                style={{
+                  color: '#EFEDE8',
+                  width: '18px',
+                  height: '18px',
+                }}
+              />
+            </button>
+          </div>
+          {showCategories && (
+            <ul className={styles.categoriesList}>
+              {categories.map(category => {
+                return (
+                  <li className={styles.categoriesListItem} key={category}>
+                    {category}
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.filtersWrapper}>
+            <input
+              className={styles.filtersBar}
+              type="text"
+              placeholder="Filter"
+              onChange={handleChange}
+            />
+            <button className={styles.chevronBtn} onClick={handleShowFilter}>
+              <IoChevronDown
+                style={{
+                  color: '#EFEDE8',
+                  width: '18px',
+                  height: '18px',
+                }}
+              />
+            </button>
+          </div>
+          {showFilters && (
+            <ul className={styles.filtersList}>
+              {filters.map(filter => {
+                return (
+                  <li className={styles.filtersListItem} key={filter}>
+                    {filter}
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
       </div>
-      
-      
-    
     </div>
   );
 }
 
 export default ProductsFilters;
-
--- src/pages/Diary/Diary.jsx
+++ src/pages/Diary/Diary.jsx
@@ -5,23 +5,25 @@ import DayExercises from 'components/diary/DayExercises';
 import DayDashboard from 'components/diary/DayDashboard';
 import Container from 'components/Container';
 import DaySwitch from 'components/diary/DaySwitch';
-import css from './Dairy.module.css';
+import styles from './Dairy.module.css';
 
 function Diary() {
   return (
-    <Container>
-      <div className={css.titleCont}>
-        <TitlePage title="Diary"></TitlePage>
-        <DaySwitch></DaySwitch>
-      </div>
-      <div className={css.container}>
-        <div className={css.itemsCont}>
-          <DayProducts></DayProducts>
-          <DayExercises></DayExercises>
+    <div className={styles.backGround}>
+      <Container>
+        <div className={styles.titleCont}>
+          <TitlePage title="Diary"></TitlePage>
+          <DaySwitch></DaySwitch>
         </div>
-        <DayDashboard></DayDashboard>
-      </div>
-    </Container>
+        <div className={styles.container}>
+          <div className={styles.itemsCont}>
+            <DayProducts></DayProducts>
+            <DayExercises></DayExercises>
+          </div>
+          <DayDashboard></DayDashboard>
+        </div>
+      </Container>
+    </div>
   );
 }
 
