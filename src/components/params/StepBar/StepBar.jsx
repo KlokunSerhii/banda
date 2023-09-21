f4ccd5b632c9e4bd4bc3635e0e31d74571e6c780
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-09-21 17:16:48 +0200

    add stepBar

--- src/components/params/StepBar/StepBar.jsx
+++ src/components/params/StepBar/StepBar.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styles from './StepBar.module.css'
+import { NavLink } from 'react-router-dom';
 
-function StepBar() {
-  return <div>StepBar</div>;
-}
+function StepBar({currentStep}) {
+
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <NavLink className={`${currentStep===0 ? styles.active : styles.visited} `} to={'/params'}></NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink className={`${currentStep===1 ? styles.active : currentStep===0 ? styles.link : styles.visited} `} to={'/params'}></NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink className={`${currentStep===2 ?styles.active: styles.link}`} to={'/params'}></NavLink>
+        </li>
+      </ul>
+    </nav>
+   
+  );
+};
 
 export default StepBar;
+
