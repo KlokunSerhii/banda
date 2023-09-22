6516a4cded6ac6967ae1021f67e9b40ae9cb5dfc
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-09-23 01:27:54 +0300

    Added ExercisesCategories, list and Item

--- src/components/exercises/ExercisesItem/ExercisesItem.jsx
+++ src/components/exercises/ExercisesItem/ExercisesItem.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import styles from './ExercisesItem.module.css';
 
-function ExercisesItem() {
-  return <div>ExercisesItem</div>;
+function ExercisesItem({ el, openModalToggle }) {
+  return (
+    <li className={styles.excercisesCard}>
+      <div className={styles.excercisesCardStatus}>
+        <span className={styles.excercisesCardDiet}>
+          <p className={styles.excercisesCardDietText}>workout</p>
+        </span>
+        <button
+          onClick={() => {
+            openModalToggle(el);
+          }}
+          className={styles.exercisesCardBtnStart}
+          type="button"
+        >
+          Start
+        </button>
+      </div>
+
+      <h4 className={styles.excercisesCardTitle}>Air bike</h4>
+      <ul className={styles.excercisesCardInfo}>
+        <li className={styles.excercisesCardInfoItem}>
+          Burned calories:<p className={styles.excercisesCardInfoValue}></p>
+        </li>
+        <li className={styles.excercisesCardInfoItem}>
+          Body part:<p className={styles.excercisesCardInfoValue}></p>
+        </li>
+        <li className={styles.excercisesCardInfoItem}>
+          Target: <p className={styles.excercisesCardInfoValue}></p>
+        </li>
+      </ul>
+    </li>
+  );
+
+  //   <h4 className={styles.excercisesCardTitle}>{el.title}</h4>
+
+  //   <ul className={styles.excercisesCardInfo}>
+  //     <li className={styles.excercisesCardInfoItem}>
+  //       Calories:
+  //       <p className={styles.excercisesCardInfoValue}>{el.calories}</p>
+  //     </li>
+  //     <li className={styles.excercisesCardInfoItem}>
+  //       Category:
+  //       <p className={styles.excercisesCardInfoValue}>{el.category}</p>
+  //     </li>
+  //     <li className={styles.excercisesCardInfoItem}>
+  //       Weight:
+  //       <p className={styles.excercisesCardInfoValue}>{el.weight}</p>
+  //     </li>
+  //   </ul>
+  // </li>
 }
 
 export default ExercisesItem;
--- src/components/exercises/ExercisesList/ExercisesList.jsx
+++ src/components/exercises/ExercisesList/ExercisesList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ExercisesItem from '../ExercisesItem';
+import styles from './ExercisesList.module.css';
 
 function ExercisesList() {
-  return <div>
-    <ExercisesItem/>
-    </div>;
+  return (
+    <ul className={styles.exercisesList}>
+      <ExercisesItem />
+    </ul>
+  );
 }
 
 export default ExercisesList;
