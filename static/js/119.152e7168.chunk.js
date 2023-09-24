a9a99a7e796d8aeedafe52f660bd296b928441b5
KlokunSerhii <99805859+KlokunSerhii@users.noreply.github.com>
2023-09-24 09:01:36 +0000

    Deploying to gh-pages from @ KlokunSerhii/banda@c56a2f9b674a38b56e570ebbbb3efdc2e694b607 🚀

--- static/js/119.152e7168.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbanda=self.webpackChunkbanda||[]).push([[119],{9119:function(e,s,n){n.r(s),n.d(s,{default:function(){return t}});n(2791);var r=n(1087),c="ExercisesItem_exercisesContainer__wNcQ8",i=n(184);var a=function(e){return e.el,e.openModalToggle,(0,i.jsx)("li",{className:c,children:(0,i.jsx)(r.OL,{to:"/exercises",children:"Hello"})})},l="ExercisesList_exercisesList__PSLFV";var t=function(){return(0,i.jsx)("ul",{className:l,children:(0,i.jsx)(a,{})})}}}]);
-//# sourceMappingURL=119.152e7168.chunk.js.map
