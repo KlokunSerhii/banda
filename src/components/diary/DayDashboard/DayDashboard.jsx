33e0e30c0216ebb2a25e016fc80ee2460f5c9541
Vira Zhuhan <115663875+ViraZhuhan@users.noreply.github.com>
2023-09-28 18:26:25 +0200

    fix products

--- src/components/diary/DayDashboard/DayDashboard.jsx
+++ src/components/diary/DayDashboard/DayDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import css from './DayDashboard.module.css';
+import styles from './DayDashboard.module.css';
 import { PiForkKnifeFill } from 'react-icons/pi';
 import { CgGym } from 'react-icons/cg';
 import { GiShinyApple } from 'react-icons/gi';
@@ -11,9 +11,9 @@ import { PiCirclesThreeFill } from 'react-icons/pi';
 function DayDashboard() {
   return (
     <div>
-      <ul className={css.container}>
-        <li className={css.box}>
-          <div className={css.header}>
+      <ul className={styles.container}>
+        <li className={styles.box}>
+          <div className={styles.header}>
             <PiForkKnifeFill
               fill="#EF8964"
               size={20}
@@ -21,63 +21,63 @@ function DayDashboard() {
             ></PiForkKnifeFill>
             <p>Daily calorie intake</p>
           </div>
-          <p className={css.result}>2200</p>
+          <p className={styles.result}>2200</p>
         </li>
-        <li className={css.box}>
-          <div className={css.header}>
+        <li className={styles.box}>
+          <div className={styles.header}>
             <CgGym fill="#EF8964" size={20} style={{ width: 20 }}></CgGym>
             <p>Daily norm of sports</p>
           </div>
-          <p className={css.result}>110 min</p>
+          <p className={styles.result}>110 min</p>
         </li>
-        <li className={css.darkBox}>
-          <div className={css.header}>
+        <li className={styles.darkBox}>
+          <div className={styles.header}>
             <GiShinyApple
               fill="#EF8964"
               size={20}
               style={{ width: 20 }}
             ></GiShinyApple>
-            <p className={css.title}>Calories consumed</p>
+            <p className={styles.title}>Calories consumed</p>
           </div>
-          <p className={css.result}>707</p>
+          <p className={styles.result}>707</p>
         </li>
-        <li className={css.darkBox}>
-          <div className={css.header}>
+        <li className={styles.darkBox}>
+          <div className={styles.header}>
             <BsFire fill="#EF8964" size={20} style={{ width: 20 }}></BsFire>
-            <p className={css.title}>Calories burned</p>
+            <p className={styles.title}>Calories burned</p>
           </div>
-          <p className={css.result}>855</p>
+          <p className={styles.result}>855</p>
         </li>
-        <li className={css.darkBox}>
-          <div className={css.header}>
+        <li className={styles.darkBox}>
+          <div className={styles.header}>
             <PiCirclesThreeFill
               fill="#EF8964"
               size={20}
               style={{ width: 20 }}
             ></PiCirclesThreeFill>
-            <p className={css.title}>Rest of the calories</p>
+            <p className={styles.title}>Rest of the calories</p>
           </div>
-          <p className={css.result}>1493</p>
+          <p className={styles.result}>1493</p>
         </li>
-        <li className={css.darkBox}>
-          <div className={css.header}>
+        <li className={styles.darkBox}>
+          <div className={styles.header}>
             <MdDirectionsRun
               fill="#EF8964"
               size={20}
               style={{ width: 20 }}
             ></MdDirectionsRun>
-            <p className={css.title}>The rest of sports</p>
+            <p className={styles.title}>The rest of sports</p>
           </div>
-          <p className={css.result}>85 min</p>
+          <p className={styles.result}>85 min</p>
         </li>
       </ul>
-      <div className={css.textCont}>
-        <PiWarningCircleBold
+      <div className={styles.textCont}>
+        <PiWarningCircleBold className={styles.warningIcon}
           fill="#EF8964"
           size={24}
-          style={{ width: 80, marginTop: 5 }}
+          style={{ width: 80}}
         ></PiWarningCircleBold>
-        <p className={css.text}>
+        <p className={styles.text}>
           Record all your meals in a calorie diary every day. This will help me
           be aware of my nutrition and make me responsible for my choices.
         </p>
--- src/components/diary/TitlePage/TitlePage.jsx
+++ src/components/diary/TitlePage/TitlePage.jsx
@@ -3,9 +3,7 @@ import css from './TitlePage.module.css';
 
 function TitlePage({ title }) {
   return (
-    <div className={css.titleContainer}>
-      <h1>{title}</h1>
-    </div>
+      <h1 className={css.titleContainer}>{title} </h1>
   );
 }
 
